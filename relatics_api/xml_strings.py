2f4f22583e0b595291f7d539346e73787f366b58
Rashied Imambaks <r@imambaks.nl>
2016-04-06 15:03:46 +0200

    removed xml-string

--- relatics_api/xml_strings.py
+++ relatics_api/xml_strings.py
@@ -23,26 +23,3 @@ import_xml = """
         </soap:Body>
     </soap:Envelope>
 """
-
-retrieve_xml = """
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-        <soap:Body>
-            <GetResult xmlns="http://www.relatics.com/">
-                <Operation>{Operation}</Operation>
-                <Identification>
-                    <Identification>
-                        <Workspace>{Workspace}</Workspace>
-                    </Identification>
-                </Identification>
-                <Authentication>
-                    <Authentication>
-                        <Entrycode>{Entrycode}</Entrycode>
-                    </Authentication>
-                </Authentication>
-            </GetResult>
-        </soap:Body>
-    </soap:Envelope>
-"""
-
